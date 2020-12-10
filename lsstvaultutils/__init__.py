4cda8de91a8ebc40249ee9aa86fb17b237152a4e
adam <athornton@gmail.com>
2020-12-10 16:57:53 -0700

    Remove unneccessary type hints

--- lsstvaultutils/__init__.py
+++ lsstvaultutils/__init__.py
@@ -1,6 +1,5 @@
-from typing import Sequence
 from .tokenadmin import AdminTool
 from .recursivedeleter import RecursiveDeleter
 from .vaultconfig import VaultConfig
 
-__all__: Sequence[str] = ["AdminTool", "RecursiveDeleter", "VaultConfig"]
+__all__ = ["AdminTool", "RecursiveDeleter", "VaultConfig"]
